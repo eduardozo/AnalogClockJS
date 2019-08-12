5918e247a7081e02a1045b6af7a81ec1519129bd
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2019-08-12 12:49:03 -0600

    Date Object gets current time

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
+
+function runTheClock() {
+    var date = new Date();
+
+    let hr = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+    console.log("Hour: " + hr + " Minute: " + min + " Seconds: " + sec);
+    
+}
+
