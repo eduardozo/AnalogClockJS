c8d843ac39263c0dd2e2419b90a68de8c59eb2db
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2019-08-22 08:21:55 -0600

    calculate position in degrees and rotate arms

--- script.js
+++ script.js
@@ -10,6 +10,16 @@ function runTheClock() {
     let min = date.getMinutes();
     let sec = date.getSeconds();
     console.log("Hour: " + hr + " Minute: " + min + " Seconds: " + sec);
-    
+
+    let hrPosition = (hr * 360 / 12) + (min * (360 / 60) / 12);
+
+    let minPosition = (min * 360 / 60) + (sec * (360 / 60) / 60);
+    let secPosition = sec * 360 / 60;
+
+
+    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+    MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+
+    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
 }
 
